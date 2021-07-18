007299185f911729bfacc128e1a8137dc9f7cbe6
aiyamia <2272682098@qq.com>
2021-07-18 10:40:02 +0800

    Ctrl+鼠标划词触发事件

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(document.body).bind('mouseup', function(e){
+    if(e.ctrlKey == true)
+    {
+      var selection;
+      
+      if (window.getSelection) {
+        selection = window.getSelection();
+      } else if (document.selection) {
+        selection = document.selection.createRange();
+      }
+      
+      selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
+  
+    }  
+  });
+});
