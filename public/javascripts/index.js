2b837dff2cbeb422c7eda2186e68dd2e59801f31
aiyamia <2272682098@qq.com>
2021-07-19 09:32:45 +0800

    完成查词功能

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,68 @@
-$(function(){
-  $(document.body).bind('mouseup', function(e){
-    if(e.ctrlKey == true)
-    {
-      var selection;
-      
-      if (window.getSelection) {
-        selection = window.getSelection();
-      } else if (document.selection) {
-        selection = document.selection.createRange();
-      }
-      $message = $(`<p id="card">${selection.toString()}</p>`);
-      $message.css({"top": e.pageY, "left": e.pageX});
-      $message.appendTo('#Content');
-      // selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
-  
-    }  
+$(document).ready(function () {
+  $(document).on('keydown', function (e) {
+    if (e.ctrlKey) 
+      $(document.body).css("cursor", "pointer");
+      var s = window.getSelection();
+      s.removeAllRanges();//取消选择
+      $("#card").remove();
   });
+  $(document).on('keyup', function (e) {
+    if (!e.ctrlKey) 
+      $(document.body).css("cursor", "initial");
+      
+  });
+  $(document.body).bind('mouseup', async function (e) {
+    if (e.ctrlKey == true) {
+      var s;
+      if (window.getSelection) {
+        s = window.getSelection();
+      } else if (document.selection) {
+        s = document.selection.createRange();
+      }
+      var range = s.getRangeAt(0);
+      var node = s.anchorNode;
+      while (range.startOffset > 0 && /[a-zA-Z]/.test(range.toString()[0])) {
+        range.setStart(node, (range.startOffset - 1));
+      }
+      if (range.startOffset != 0)
+        range.setStart(node, range.startOffset + 1);
+      do {
+        range.setEnd(node, range.endOffset + 1);
+      }
+      while (/[a-zA-Z]/.test(range.toString().slice(-1)));
+      range.setEnd(node, range.endOffset - 1);
+      var word = range.toString().trim();
+      if (word != '') {
+        $card = await getCardNode(word);
+        $card.css({
+          "top": e.pageY,
+          "left": e.pageX
+        });
+      }
+    }
+  });
+  const getCardNode = async(word)=>{
+    const data = await fetchData(word);
+    $card = $('<div id="card"></div>');
+    $pron = $('<div class="pronunciation"></div>');
+    $mean = $('<div class="meaning"></div>');
+    data.meaning.forEach(element => {
+      const mean_str = element.ps+ " " + element.exp.join("；");
+      $mean.append( `<p>${mean_str}</p>` );
+    });
+    $pron.append( `/${data.pronunciation.BrE}/` );
+    $card.append($pron);
+    $card.append($mean);
+    return $card;
+  };
+  const fetchData = async(word)=>{
+    let response = await fetch(`https://dictweb.translator.qq.com/api/elementary?word=${word}`);
+    let result = await response.json();
+    console.log(result);
+    let data = {
+      meaning:result.oxford_dict_info.abstract,
+      pronunciation:result.oxford_dict_info.ph_json
+    };
+    return data;
+  };
 });
